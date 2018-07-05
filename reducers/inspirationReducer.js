9beeadd0e8197d061be169af3cfa6560cf212a16
Marina Dargitz <marina.dargitz@gmail.com>
2018-07-05 15:57:43 -0600

    Add inspiration reducers

--- reducers/inspirationReducer.js
+++ reducers/inspirationReducer.js
@@ -1,3 +1,5 @@
+import { REQUEST_ART, ART_SUCCESS, ART_ERROR } from "../actions/inspiration";
+
 const initialState = {
   art : [],
   loading: false,
@@ -5,10 +7,29 @@ const initialState = {
 };
 
 export default function inspirationReducer(state = initialState, action) {
+  if (action.type === REQUEST_ART){
+    return {
+      ...state,
+      loading: true
+    };
+  }
 
+  if (action.type === ART_SUCCESS){
+    return {
+      ...state,
+      art: action.data,
+      error: null,
+      loading: false
+    };
+  }
+
+  if (action.type === ART_ERROR){
+    return {
+      ...state,
+      error: action.error,
+      loading: false
+    };
+  }
   
-
-
-
   return state;
 }
